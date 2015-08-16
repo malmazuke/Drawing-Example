b49f991b9b7c79215b32421378161906dd75db79
Mark Feaver <mark.feaver@gmail.com>
2015-08-16 17:50:59 +1200

    Added brush size + opacity settings

--- DrawPad/SettingsViewController.swift
+++ DrawPad/SettingsViewController.swift
@@ -19,6 +19,9 @@ class SettingsViewController: UIViewController {
   @IBOutlet weak var labelGreen: UILabel!
   @IBOutlet weak var labelBlue: UILabel!
   
+  var brush: CGFloat = 10.0
+  var opacity: CGFloat = 1.0
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
@@ -38,16 +41,43 @@ class SettingsViewController: UIViewController {
   }
 
   @IBAction func sliderChanged(sender: UISlider) {
+    if sender == sliderBrush {
+      brush = CGFloat(sender.value)
+      labelBrush.text = NSString(format: "%.2f", brush.native) as String
+    } else {
+      opacity = CGFloat(sender.value)
+      labelOpacity.text = NSString(format: "%.2f", opacity.native) as String
+    }
+    
+    drawPreview()
   }
 
-  /*
-  // MARK: - Navigation
-
-  // In a storyboard-based application, you will often want to do a little preparation before navigation
-  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-  // Get the new view controller using segue.destinationViewController.
-  // Pass the selected object to the new view controller.
+  func drawPreview() {
+    UIGraphicsBeginImageContext(imageViewBrush.frame.size)
+    var context = UIGraphicsGetCurrentContext()
+    
+    CGContextSetLineCap(context, kCGLineCapRound)
+    CGContextSetLineWidth(context, brush)
+    
+    CGContextSetRGBStrokeColor(context, 0.0, 0.0, 0.0, 1.0)
+    CGContextMoveToPoint(context, 45.0, 45.0)
+    CGContextAddLineToPoint(context, 45.0, 45.0)
+    CGContextStrokePath(context)
+    imageViewBrush.image = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    
+    UIGraphicsBeginImageContext(imageViewBrush.frame.size)
+    context = UIGraphicsGetCurrentContext()
+    
+    CGContextSetLineCap(context, kCGLineCapRound)
+    CGContextSetLineWidth(context, 20)
+    CGContextMoveToPoint(context, 45.0, 45.0)
+    CGContextAddLineToPoint(context, 45.0, 45.0)
+    
+    CGContextSetRGBStrokeColor(context, 0.0, 0.0, 0.0, opacity)
+    CGContextStrokePath(context)
+    imageViewOpacity.image = UIGraphicsGetImageFromCurrentImageContext()
+    
+    UIGraphicsEndImageContext()
   }
-  */
-
 }
